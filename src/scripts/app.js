be4e4cd053830719d81baccd59bd8126e5f12b52
Caitlin Flattery <caitlinflattery@gmail.com>
2016-11-17 11:49:54 -0600

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,11 +5,30 @@ import init from './init'
 
 
 const app = function() {
-  document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+
+// setting up MVC
+//* create a view for the home page
+//* set up a router
+//* read api docs and set up models to manage data
+
+// set up flux
+//* create actions module
+//* create store (pub)
+//* tell view to become a (sub)
+
+// create backend for faves
+//* set up schema for a fave
+//* set up api routes
+
+// integrate front-end with backend
+// * create actions to store faves (model.save /POST)
+// * create actions to fetch faves (coll.fetch /GET)
+
+//use a css framework
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
